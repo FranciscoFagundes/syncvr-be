59ea1b6fde542b2bf3aeaed563acbb11d64220a9
Francisco Fagundes <francisco.fagundes@bluepanda.pt>
2022-06-08 15:46:06 +0100

    make it better

---
+++ db.js
@@ -0,0 +1,29 @@
+async function connect() {
+    if (global.connection && global.connection.state != 'disconnected')
+        return global.connection;
+    const mysql = require("mysql2/promise");
+    const connection = await mysql.createConnection("mysql://bfc91110e0c25b:583bfb0f@eu-cdbr-west-02.cleardb.net/heroku_eec4ae15216c394?reconnect=true");
+    console.log("connected to database");
+    global.connection = connection;
+    return connection;
+}
+
+
+async function getList() {
+    const conn = await connect();
+    let query = 'SELECT * FROM log';
+
+    return conn.query(query).then(([rows]) => {
+        return rows;
+    }).catch(error => {
+        throw error;
+    })
+}
+
+async function addRequisition(fibonacciNumber, numberPosition, requisitionDate) {
+    const conn = await connect();
+    return await conn.query("INSERT INTO log (fibonacci_number, number_position, requisition_date) values  (" + "'" + fibonacciNumber + "','" + numberPosition + "','" + requisitionDate + "')");
+}
+
+
+module.exports = { connect, getList, addRequisition }
---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+const db = require("./db");
+
+app.use(cors());
+
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
+
+app.post('/numberPosition', async (req, res) => {
+    let numberPosition = req.query.number;
+    let fibonacciNumber = findFibonacci(numberPosition)
+    db.addRequisition(fibonacciNumber, numberPosition, new Date().toISOString().slice(0, 19).replace('T', ' '));
+    res.json(fibonacciNumber);
+});
+
+app.get('/getList', async (req, res) => {
+    res.json(await db.getList()).end();
+});
+
+const findFibonacci = (numberPosition)=>{
+    let fibonacci = [0,1];
+    for (let i = 1; i < numberPosition - 1; i++) {
+        fibonacci.push(fibonacci[i] + fibonacci[i - 1]);
+    }
+    return fibonacci[fibonacci.length - 1] ;
+}
+  
